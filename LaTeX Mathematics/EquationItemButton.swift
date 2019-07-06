3791025ffc78add6b218771c97ebfdd97abdac0a
Jiaxin Shou <shoujiaxin@gmail.com>
2019-07-06 21:23:17 +0800

    Add tab view controller and custom button

---
+++ LaTeX Mathematics/EquationItemButton.swift
@@ -0,0 +1,32 @@
+//
+//  EquationItemButton.swift
+//  LaTeX Mathematics
+//
+//  Created by 寿佳鑫 on 2019/7/6.
+//  Copyright © 2019 Jiaxin Shou. All rights reserved.
+//
+
+import Cocoa
+
+class EquationItemButton: NSButton {
+    override func viewWillDraw() {
+        imageScaling = NSImageScaling.scaleNone
+        image?.size = NSSize(width: frame.width / 2, height: frame.height / 2)
+
+        layer?.cornerRadius = 6
+        layer?.borderWidth = 0 // Hide border by default
+        layer?.borderColor = NSColor.controlAccentColor.cgColor
+
+        // Add tracking area to show border when mouse hover
+        let trackingArea = NSTrackingArea(rect: bounds, options: [NSTrackingArea.Options.mouseEnteredAndExited, NSTrackingArea.Options.activeAlways], owner: self, userInfo: nil)
+        addTrackingArea(trackingArea)
+    }
+
+    override func mouseEntered(with _: NSEvent) {
+        layer?.borderWidth = 3
+    }
+
+    override func mouseExited(with _: NSEvent) {
+        layer?.borderWidth = 0
+    }
+}
---
+++ LaTeX Mathematics/EquationTabViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EquationTabViewController.swift
+//  LaTeX Mathematics
+//
+//  Created by 寿佳鑫 on 2019/7/6.
+//  Copyright © 2019 Jiaxin Shou. All rights reserved.
+//
+
+import Cocoa
+
+class EquationTabViewController: NSTabViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+}
