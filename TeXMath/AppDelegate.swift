2401da1ccc88acd1e70884d1e7233b97d60ae37f
Jiaxin Shou <shoujiaxin@gmail.com>
2019-11-13 16:32:32 +0800

    Add new project

---
+++ TeXMath/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TeXMath
+//
+//  Created by Jiaxin Shou on 2019/11/13.
+//  Copyright © 2019 Jiaxin Shou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TeXMath/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TeXMath
+//
+//  Created by Jiaxin Shou on 2019/11/13.
+//  Copyright © 2019 Jiaxin Shou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
