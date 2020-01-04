0c3722d182a2dac06c0506e5e61fd81b7cf78582
Jiaxin Shou <shoujiaxin@gmail.com>
2020-01-04 22:47:09 +0800

    Replace NSGridView with NSCollectionView in accent tab

--- TeXMath/ViewControllers/AccentViewController.swift
+++ TeXMath/ViewControllers/AccentViewController.swift
@@ -9,59 +9,59 @@
 import Cocoa
 
 class AccentViewController: NSViewController {
+    @IBOutlet var collectionView: NSCollectionView!
+
+    var items = EquationItems()
+
+    override func viewWillLayout() {
+        collectionView.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        items.addSection(withName: "Accents")
+        items.sections[0].addItem(withName: "Dot", imageName: "DotButtonIcon", latexCode: "\\dot{} ")
+        items.sections[0].addItem(withName: "Double Dot", imageName: "DoubleDotButtonIcon", latexCode: "\\ddot{} ")
+        items.sections[0].addItem(withName: "Triple Dot", imageName: "TripleDotButtonIcon", latexCode: "\\dddot{} ")
+        items.sections[0].addItem(withName: "Hat", imageName: "HatButtonIcon", latexCode: "\\hat{} ")
+        items.sections[0].addItem(withName: "Check", imageName: "CheckButtonIcon", latexCode: "\\check{} ")
+        items.sections[0].addItem(withName: "Acute", imageName: "AcuteButtonIcon", latexCode: "\\acute{} ")
+        items.sections[0].addItem(withName: "Grave", imageName: "GraveButtonIcon", latexCode: "\\grave{} ")
+        items.sections[0].addItem(withName: "Breve", imageName: "BreveButtonIcon", latexCode: "\\breve{} ")
+        items.sections[0].addItem(withName: "Tilde", imageName: "TildeButtonIcon", latexCode: "\\tilde{} ")
+        items.sections[0].addItem(withName: "Bar", imageName: "BarButtonIcon", latexCode: "\\bar{} ")
+        items.sections[0].addItem(withName: "Vector", imageName: "VectorButtonIcon", latexCode: "\\vec{} ")
+        items.sections[0].addItem(withName: "Overbrace", imageName: "OverbraceButtonIcon", latexCode: "\\overbrace{} ")
+        items.sections[0].addItem(withName: "Underbrace", imageName: "UnderbraceButtonIcon", latexCode: "\\underbrace{} ")
+    }
+}
+
+extension AccentViewController: NSCollectionViewDataSource {
+    func numberOfSections(in _: NSCollectionView) -> Int {
+        return items.numberOfSections
     }
 
-    @IBAction func dotButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\dot{} ")
+    func collectionView(_: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.numberOfItems(in: section)
     }
 
-    @IBAction func doubleDotButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\ddot{} ")
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        let item = collectionView.makeItem(withIdentifier: .init("CollectionViewItem"), for: indexPath)
+        guard let collectionViewItem = item as? CollectionViewItem else {
+            return item
+        }
+
+        collectionViewItem.itemInfo = items.item(in: indexPath.section, at: indexPath.item)
+
+        return collectionViewItem
     }
 
-    @IBAction func tripleDotButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\dddot{} ")
-    }
+    func collectionView(_ collectionView: NSCollectionView, viewForSupplementaryElementOfKind _: NSCollectionView.SupplementaryElementKind, at indexPath: IndexPath) -> NSView {
+        let view = collectionView.makeSupplementaryView(ofKind: NSCollectionView.elementKindSectionHeader, withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "SectionHeaderView"), for: indexPath) as! SectionHeaderView
 
-    @IBAction func hatButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\hat{} ")
-    }
+        view.sectionTitle.stringValue = items.sections[indexPath.section].name!
 
-    @IBAction func checkButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\check{} ")
-    }
-
-    @IBAction func acuteButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\acute{} ")
-    }
-
-    @IBAction func graveButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\grave{} ")
-    }
-
-    @IBAction func breveButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\breve{} ")
-    }
-
-    @IBAction func tildeButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\tilde{} ")
-    }
-
-    @IBAction func barButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\bar{} ")
-    }
-
-    @IBAction func vectorButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\vec{} ")
-    }
-
-    @IBAction func overbraceButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\overbrace{} ")
-    }
-
-    @IBAction func underbraceButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\underbrace{} ")
+        return view
     }
 }
