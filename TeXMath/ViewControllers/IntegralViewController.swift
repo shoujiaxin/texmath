0acab674ad199c00213cb80435ca51a44581d032
Jiaxin Shou <shoujiaxin@gmail.com>
2020-01-04 21:12:21 +0800

    Replace NSGridView with NSCollectionView in integral tab

--- TeXMath/ViewControllers/IntegralViewController.swift
+++ TeXMath/ViewControllers/IntegralViewController.swift
@@ -9,39 +9,42 @@
 import Cocoa
 
 class IntegralViewController: NSViewController {
+    @IBOutlet var collectionView: NSCollectionView!
+
+    var items = EquationItems()
+
+    override func viewWillLayout() {
+        collectionView.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        items.addSection(withName: "")
+        items.sections[0].addItem(withName: "Integral", imageName: "IntegralButtonIcon", latexCode: "\\int{} ")
+        items.sections[0].addItem(withName: "Integral with Limits", imageName: "IntegralWithLimitsButtonIcon", latexCode: "\\int_{}^{}{} ")
+        items.sections[0].addItem(withName: "Double Integral", imageName: "DoubleIntegralButtonIcon", latexCode: "\\iint{} ")
+        items.sections[0].addItem(withName: "Double Integral with Limits", imageName: "DoubleIntegralWithLimitsButtonIcon", latexCode: "\\iint_{}^{}{} ")
+        items.sections[0].addItem(withName: "Triple Integral", imageName: "TripleIntegralButtonIcon", latexCode: "\\iiint{} ")
+        items.sections[0].addItem(withName: "Triple Integral with Limits", imageName: "TripleIntegralWithLimitsButtonIcon", latexCode: "\\iiint_{}^{}{} ")
+        items.sections[0].addItem(withName: "Contour Integral", imageName: "ContourIntegralButtonIcon", latexCode: "\\oint{} ")
+        items.sections[0].addItem(withName: "Contour Integral with Limits", imageName: "ContourIntegralWithLimitsButtonIcon", latexCode: "\\oint_{}^{}{} ")
+    }
+}
+
+extension IntegralViewController: NSCollectionViewDataSource {
+    func collectionView(_: NSCollectionView, numberOfItemsInSection _: Int) -> Int {
+        return items.numberOfItems(inSection: 0)
     }
 
-    @IBAction func integralButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\int{} ")
-    }
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        let item = collectionView.makeItem(withIdentifier: .init("CollectionViewItem"), for: indexPath)
+        guard let collectionViewItem = item as? CollectionViewItem else {
+            return item
+        }
 
-    @IBAction func integralWithLimitsButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\int_{}^{}{} ")
-    }
+        collectionViewItem.itemInfo = items.item(inSection: indexPath.section, atIndex: indexPath.item)
 
-    @IBAction func doubleIntegralButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\iint{} ")
-    }
-
-    @IBAction func doubleIntegralWithLimitsButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\iint_{}^{}{} ")
-    }
-
-    @IBAction func tripleIntegralButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\iiint{} ")
-    }
-
-    @IBAction func tripleIntegralWithLimitsButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\iiint_{}^{}{} ")
-    }
-
-    @IBAction func contourIntegralButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\oint{} ")
-    }
-
-    @IBAction func contourIntegralWithLimitsButtonClicked(_: Any) {
-        Utils.insertLatex(latexCode: "\\oint_{}^{}{} ")
+        return collectionViewItem
     }
 }
